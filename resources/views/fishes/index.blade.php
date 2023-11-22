7c3ae6cec5c5afb7e539d5664c8f443a40a6b357
D1104181039 <peter56612@gmail.com>
2023-11-22 14:42:06 +0800

    view

--- resources/views/fishes/index.blade.php
+++ resources/views/fishes/index.blade.php
@@ -1,3 +1,22 @@
-<div>
-    <!-- The whole future lies in uncertainty: live immediately. - Seneca -->
-</div>
+<html>
+<head>
+                <title>列出所有魚</title>
+</head>
+<body>
+    <h1>列出所有魚</h1>
+@for($i=0; $i<count($fishes); $i++)
+    {{ $fishes[$i]['name'] }} <br/>
+    {{ $fishes[$i]['sid'] }} <br/>
+    {{ $fishes[$i]['longest'] }} <br/>
+    {{ $fishes[$i]['shortest'] }} <br/><br/>
+    {{ $fishes[$i]['start'] }} <br/><br/>
+    {{ $fishes[$i]['end'] }} <br/><br/>
+    {{ $fishes[$i]['lightest_weight'] }} <br/><br/>
+    {{ $fishes[$i]['heaviest_weight'] }} <br/><br/>
+@endfor
+
+
+
+
+</body> 
+</html>
