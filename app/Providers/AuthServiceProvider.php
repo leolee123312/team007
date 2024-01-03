a95b3df840faabf60573bb2dc2e2b180a6db1e26
D1104181039 <peter56612@gmail.com>
2024-01-03 14:24:08 +0800

    建立Gate權限規則

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,9 +4,24 @@ namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use App\User;
 
 class AuthServiceProvider extends ServiceProvider
 {
+    public function boot()
+    {
+        $this->registerPolicies();
+
+        Gate::define('admin',function($user){
+            return $user->role === User::ROLE_ADMIN;
+        });
+        Gate::define('manager',function($user){
+            return $user->role === User::ROLE_MANAGER;
+        });
+        Gate::define('user',function($user){
+            return $user->role === User::ROLE_USER;
+        });
+    }
     /**
      * The model to policy mappings for the application.
      *
@@ -19,8 +34,5 @@ class AuthServiceProvider extends ServiceProvider
     /**
      * Register any authentication / authorization services.
      */
-    public function boot(): void
-    {
-        //
-    }
+
 }
