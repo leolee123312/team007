81ad1443bbf476ab168d42645119fd2d45f05567
D1104181039 <peter56612@gmail.com>
2024-01-03 14:21:42 +0800

    區分使用者，管理者，系統管理者

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,17 +11,18 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+    const ROLE_ADMIN = 'admin';
+    const ROLE_MANAGER = 'admin';
+    const ROLE_USER = 'admin';
 
+    protected $fillable = [
+        'name','email','password','role',
+    ];
     /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
 
     /**
      * The attributes that should be hidden for serialization.
