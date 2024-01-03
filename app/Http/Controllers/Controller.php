d07bf9e61a20fc5eed645ef6ae4d0857b9866c21
D1104181039 <peter56612@gmail.com>
2024-01-03 14:25:14 +0800

    Controller運用Gate權限設定

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -8,5 +8,18 @@ use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
+    public function someAction()
+    {
+        if(Gate::allows('admin')){
+            return'系統管理者。';
+        }
+        if(Gate::denise('admin')){
+            return'非系統管理者。';
+        }
+    }
+    public function adminAction()
+    {
+        $this->authorize('admin');
+    }
     use AuthorizesRequests, ValidatesRequests;
 }
